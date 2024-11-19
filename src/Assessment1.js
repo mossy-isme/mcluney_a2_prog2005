2cf139974938b5b1a2c7f85fb5ae631a1235d6c0
mossy-isme <mossycluney@gmail.com>
2024-11-19 23:29:39 +1100

    modified:   css/index.css - changed disabled styling 	modified:   index.html - changed event type for submission of search query 	modified:   src/Assessment1.js 	modified:   src/Assessment1.ts - fixed various bugs, added deleting and editing 	new file:   src/tsconfig.json

--- src/Assessment1.js
+++ src/Assessment1.js
@@ -13,16 +13,28 @@ var Client = /** @class */ (function () {
         this.specialHealthNotes = specialHealthNotes;
     }
     Client.prototype.displayClientInfo = function () {
+        var _a;
         if (this.contactInfo.email == null) {
-            var emailGiven = "<em>None given</em>";
+            var emailGiven = "None given";
         }
         else {
             var emailGiven = this.contactInfo.email;
         }
         ;
-        return "\n        <div class=\"client-card\">\n          <h3>".concat(this.name, " (").concat(this.clientID, ")</h3>\n          <p><strong>Gender:</strong> ").concat(this.gender, "</p>\n          <p><strong>Fitness Program:</strong> ").concat(this.fitnessProgram, "</p>\n          <p><strong>Address:</strong> ").concat(this.contactInfo.address, "</p>\n          <p><strong>Phone Number:</strong> ").concat(this.contactInfo.phoneNumber, "</p>\n          <p><strong>Email:</strong> ").concat(emailGiven, "</p>\n          <p><strong>Joined Date:</strong> ").concat(this.joinedDate.toLocaleDateString(), "</p>\n          <p><strong>Ending Date:</strong> ").concat(this.endingDate.toLocaleDateString(), "</p>\n          ").concat(this.specialHealthNotes
-            ? "<p><strong>Notes:</strong> ".concat(this.specialHealthNotes, "</p>")
-            : '', "\n          <p><strong>VIP Client:</strong> ").concat(this.isVIP ? 'Yes' : 'No', "</p>\n        </div>\n      ");
+        if (this.isVIP ? true : false) {
+            var checked = "checked";
+        }
+        else {
+            var checked = "";
+        }
+        ;
+        if (this.specialHealthNotes != "") {
+            var notes = (_a = this.specialHealthNotes) === null || _a === void 0 ? void 0 : _a.toString();
+        }
+        else {
+            var notes = "None given";
+        }
+        return "\n        <div class=\"client-card\" id=\"client-card-".concat(this.clientID, "\">\n\n          <h3><input class=\"editable\" type=\"text\" id=\"name-").concat(this.clientID, "\" value=\"").concat(this.name, "\" disabled/> (").concat(this.clientID, ")</h3>\n          <button style=\"width:30%\" onclick=\"editClient(").concat(this.clientID, ")\">Edit Client</button>\n          <button style=\"width:30%\" onclick=\"deleteClient(").concat(this.clientID, ")\">Delete Client</button>\n\n          <p style=\"float:none\"><strong>Gender: </strong><select class=\"editable\" id=\"gender-").concat(this.clientID, "\" value=\"").concat(this.gender, "\" disabled>\n          <option value=\"Male\">Male</option>\n          <option value=\"Female\">Female</option>\n          <option value=\"Unspecified\">Unspecified</option></select\n        ></p>\n\n          <p><strong>Fitness Program: </strong><select class=\"editable\" id=\"fitnessProgram-").concat(this.clientID, "\" value=\"").concat(this.fitnessProgram, "\" disabled>\n          <option value=\"fat loss\">Fat loss</option>\n          <option value=\"senior fitness\">Senior fitness</option>\n          <option value=\"muscle gain\">Muscle gain</option>\n          <option value=\"pre/postnatal fitness\">Pre/Postnatal fitness</option>\n          <option value=\"contest preparation\">Contest preparation</option>\n          <option value=\"overall fitness\">Overall fitness</option></select\n        ></p>\n          \n          <p><strong>Address: </strong><input type=\"text\" class=\"editable\" id=\"address-").concat(this.clientID, "\" value=\"").concat(this.contactInfo.address, "\" required disabled/></p>\n          <p><strong>Phone Number: </strong><input type=\"text\" class=\"editable\" id=\"phoneNumber-").concat(this.clientID, "\" value=\"").concat(this.contactInfo.phoneNumber, "\" required disabled/></p>\n          <p><strong>Email: </strong><input type=\"email\" class=\"editable\" id=\"email-").concat(this.clientID, "\" value=\"").concat(emailGiven, "\" disabled/>\n </p>\n          <p><strong>DOB: </strong> ").concat(this.DOB.toLocaleDateString(), "</p>\n          <p><strong>Joined Date: </strong> ").concat(this.joinedDate.toLocaleDateString(), "</p>\n          <p><strong>Ending Date: </strong> ").concat(this.endingDate.toLocaleDateString(), "</p>\n          <p><strong>Notes: </strong><textarea class=\"editable\" id=\"specialHealthNotes-").concat(this.clientID, "\" value=\"").concat(notes, "\" disabled></textarea></p>\n          <p><strong>VIP Client: </strong><input type=\"checkbox\" class=\"editable checkbox\" id=\"isVIP-").concat(this.clientID, "\" ").concat(checked, " disabled/></p>\n          <button id=\"updateButton\" class=\"editable\" onclick=\"updateClient(").concat(this.clientID, ")\" disabled>Update details</button>\n        </div>\n      ");
     };
     return Client;
 }());
@@ -51,7 +63,6 @@ function addClient() {
     }
     var client = new Client(isVIP, clientID, name, DOB, gender, fitnessProgram, { phoneNumber: phoneNumber, address: address, email: email }, joinedDate, endingDate, specialHealthNotes);
     clients.push(client);
-    displayClients();
     displayMessage('Client added successfully!', 'success');
 }
 // Display all clients
@@ -71,17 +82,13 @@ function displayVIPClients() {
 }
 // case insensitive search of n-number properties of type T
 // returns true if at least one of the property values includes the query value
-function clientSearch(object, properties, query) {
-    if (query === "") {
-        return true;
-    }
-    return properties.some(function (property) {
-        var value = object[property];
-        if (typeof value === "string" || typeof value === "number") {
-            return value.toString().toLowerCase().includes(query.toLowerCase());
-        }
-        return false;
-    });
+function clientIDSearch() {
+    var id = document.getElementById("searchQuery").value;
+    var display = document.getElementById('clientList');
+    display.innerHTML = clients
+        .filter(function (client) { return client.clientID.toString() == id; })
+        .map(function (client) { return client.displayClientInfo(); })
+        .join('');
 }
 // Display a message
 function displayMessage(message, type) {
@@ -91,31 +98,22 @@ function displayMessage(message, type) {
     document.body.prepend(msgDiv);
     setTimeout(function () { return msgDiv.remove(); }, 3000);
 }
-//  Update client data
-function updateClient() {
-    var clientID = Number(document.getElementById('clientID').value);
-    var name = document.getElementById('name').value;
-    var DOB = new Date(document.getElementById('dob').value);
-    var gender = document.getElementById('gender')
+//Edit client
+function editClient(id) {
+    document.querySelectorAll("#client-card-" + id.toString() + " *:disabled").forEach(function (input) { return input.removeAttribute("disabled"); });
+}
+//Update client
+function updateClient(id) {
+    document.querySelectorAll("#client-card-" + id.toString() + " .editable").forEach(function (input) { return input.setAttribute("disabled", ""); });
+    var name = document.getElementById('name-' + id.toString()).value;
+    var gender = document.getElementById('gender-' + id.toString())
         .value;
-    var fitnessProgram = document.getElementById('fitnessProgram').value;
-    var phoneNumber = document.getElementById('phoneNumber').value;
-    var address = document.getElementById('address')
+    var fitnessProgram = document.getElementById('fitnessProgram-' + id.toString()).value;
+    var phoneNumber = document.getElementById('phoneNumber-' + id.toString()).value;
+    var address = document.getElementById('address-' + id.toString())
         .value;
-    var email = document.getElementById('email').value || undefined;
-    var joinedDate = new Date(document.getElementById('joinedDate').value);
-    var endingDate = new Date(document.getElementById('endingDate').value);
-    var specialHealthNotes = document.getElementById('specialHealthNotes')
+    var email = document.getElementById('email-' + id.toString()).value || undefined;
+    var specialHealthNotes = document.getElementById('specialHealthNotes-' + id.toString())
         .value || undefined;
-    var isVIP = document.getElementById('isVIP').checked;
-    for (var i in clients) {
-        if (i == clientID.toString()) {
-            displayMessage('Client ID must be unique!', 'error');
-            return;
-        }
-    }
-    var client = new Client(isVIP, clientID, name, DOB, gender, fitnessProgram, { phoneNumber: phoneNumber, address: address, email: email }, joinedDate, endingDate, specialHealthNotes);
-    clients.push(client);
-    displayClients();
-    displayMessage('Client added successfully!', 'success');
+    var isVIP = document.getElementById('isVIP-' + id.toString()).checked;
 }
--- src/Assessment1.ts
+++ src/Assessment1.ts
@@ -1,248 +1,248 @@
 // Define the Client class
 class Client {
-    isVIP: boolean;
-    clientID: number;
-    name: string;
-    DOB: Date;
-    gender: 'Male' | 'Female' | 'Unspecified';
+  isVIP: boolean;
+  clientID: number;
+  name: string;
+  DOB: Date;
+  gender: 'Male' | 'Female' | 'Unspecified';
+  fitnessProgram:
+    | 'fat loss'
+    | 'senior fitness'
+    | 'muscle gain'
+    | 'pre/postnatal fitness'
+    | 'contest preparation'
+    | 'overall fitness';
+  contactInfo: {
+    phoneNumber: string;
+    address: string;
+    email?: string;
+  };
+  joinedDate: Date;
+  endingDate: Date;
+  specialHealthNotes?: string;
+
+  constructor(
+    isVIP: boolean,
+    clientID: number,
+    name: string,
+    DOB: Date,
+    gender: 'Male' | 'Female' | 'Unspecified',
     fitnessProgram:
       | 'fat loss'
       | 'senior fitness'
       | 'muscle gain'
       | 'pre/postnatal fitness'
       | 'contest preparation'
-      | 'overall fitness';
+      | 'overall fitness',
     contactInfo: {
       phoneNumber: string;
       address: string;
       email?: string;
+    },
+    joinedDate: Date,
+    endingDate: Date,
+    specialHealthNotes?: string
+  ) {
+    this.isVIP = isVIP;
+    this.clientID = clientID;
+    this.name = name;
+    this.DOB = DOB;
+    this.gender = gender;
+    this.fitnessProgram = fitnessProgram;
+    this.contactInfo = contactInfo;
+    this.joinedDate = joinedDate;
+    this.endingDate = endingDate;
+    this.specialHealthNotes = specialHealthNotes;
+  }
+
+  displayClientInfo(): string {
+    if (this.contactInfo.email == null) {
+      var emailGiven = "None given"
+    } else {
+      var emailGiven = this.contactInfo.email
     };
-    joinedDate: Date;
-    endingDate: Date;
-    specialHealthNotes?: string;
-  
-    constructor(
-      isVIP: boolean,
-      clientID: number,
-      name: string,
-      DOB: Date,
-      gender: 'Male' | 'Female' | 'Unspecified',
-      fitnessProgram:
-        | 'fat loss'
-        | 'senior fitness'
-        | 'muscle gain'
-        | 'pre/postnatal fitness'
-        | 'contest preparation'
-        | 'overall fitness',
-      contactInfo: {
-        phoneNumber: string;
-        address: string;
-        email?: string;
-      },
-      joinedDate: Date,
-      endingDate: Date,
-      specialHealthNotes?: string
-    ) {
-      this.isVIP = isVIP;
-      this.clientID = clientID;
-      this.name = name;
-      this.DOB = DOB;
-      this.gender = gender;
-      this.fitnessProgram = fitnessProgram;
-      this.contactInfo = contactInfo;
-      this.joinedDate = joinedDate;
-      this.endingDate = endingDate;
-      this.specialHealthNotes = specialHealthNotes;
+    if (this.isVIP ? true : false) {
+      var checked = "checked"
+    } else {
+      var checked = ""
+    };
+    if (this.specialHealthNotes != "") {
+      var notes = this.specialHealthNotes?.toString()
+    } else {
+      var notes = "None given"
     }
-  
-    displayClientInfo(): string {
-      if (this.contactInfo.email == null) {
-        var emailGiven = "<em>None given</em>"
-      } else {
-        var emailGiven = this.contactInfo.email
-      };
-      return `
-        <div class="client-card">
-          <h3>${this.name} (${this.clientID})</h3>
-          <p><strong>Gender:</strong> ${this.gender}</p>
-          <p><strong>Fitness Program:</strong> ${this.fitnessProgram}</p>
-          <p><strong>Address:</strong> ${this.contactInfo.address}</p>
-          <p><strong>Phone Number:</strong> ${this.contactInfo.phoneNumber}</p>
-          <p><strong>Email:</strong> ${emailGiven}</p>
-          <p><strong>Joined Date:</strong> ${this.joinedDate.toLocaleDateString()}</p>
-          <p><strong>Ending Date:</strong> ${this.endingDate.toLocaleDateString()}</p>
-          ${
-            this.specialHealthNotes
-              ? `<p><strong>Notes:</strong> ${this.specialHealthNotes}</p>`
-              : ''
-          }
-          <p><strong>VIP Client:</strong> ${this.isVIP ? 'Yes' : 'No'}</p>
+    return `
+        <div class="client-card" id="client-card-${this.clientID}">
+
+          <h3><input class="editable" type="text" id="name-${this.clientID}" value="${this.name}" disabled/> (${this.clientID})</h3>
+          <button style="width:30%" onclick="editClient(${this.clientID})">Edit Client</button>
+          <button style="width:30%" onclick="deleteClient(${this.clientID})">Delete Client</button>
+
+          <p style="float:none"><strong>Gender: </strong><select class="editable" id="gender-${this.clientID}" value="${this.gender}" disabled>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Unspecified">Unspecified</option></select
+        ></p>
+
+          <p><strong>Fitness Program: </strong><select class="editable" id="fitnessProgram-${this.clientID}" value="${this.fitnessProgram}" disabled>
+          <option value="fat loss">Fat loss</option>
+          <option value="senior fitness">Senior fitness</option>
+          <option value="muscle gain">Muscle gain</option>
+          <option value="pre/postnatal fitness">Pre/Postnatal fitness</option>
+          <option value="contest preparation">Contest preparation</option>
+          <option value="overall fitness">Overall fitness</option></select
+        ></p>
+          
+          <p><strong>Address: </strong><input type="text" class="editable" id="address-${this.clientID}" value="${this.contactInfo.address}" required disabled/></p>
+          <p><strong>Phone Number: </strong><input type="text" class="editable" id="phoneNumber-${this.clientID}" value="${this.contactInfo.phoneNumber}" required disabled/></p>
+          <p><strong>Email: </strong><input type="email" class="editable" id="email-${this.clientID}" value="${emailGiven}" disabled/>
+ </p>
+          <p><strong>DOB: </strong> ${this.DOB.toLocaleDateString()}</p>
+          <p><strong>Joined Date: </strong> ${this.joinedDate.toLocaleDateString()}</p>
+          <p><strong>Ending Date: </strong> ${this.endingDate.toLocaleDateString()}</p>
+          <p><strong>Notes: </strong><textarea class="editable" id="specialHealthNotes-${this.clientID}" value="${notes}" disabled></textarea></p>
+          <p><strong>VIP Client: </strong><input type="checkbox" class="editable checkbox" id="isVIP-${this.clientID}" ${checked} disabled/></p>
+          <button id="updateButton" class="editable" onclick="updateClient(${this.clientID})" disabled>Update details</button>
         </div>
       `;
-    }
   }
-  
-  // Database of clients
-  let clients: Client[] = [];
-  
-  // Add client
-  function addClient(): void {
-    const clientID = Number(
-      (document.getElementById('clientID') as HTMLInputElement).value
-    );
-    const name = (document.getElementById('name') as HTMLInputElement).value;
-    const DOB = new Date(
-      (document.getElementById('dob') as HTMLInputElement).value
-    );
-    const gender = (document.getElementById('gender') as HTMLSelectElement)
-      .value as 'Male' | 'Female' | 'Unspecified';
-    const fitnessProgram = (
-      document.getElementById('fitnessProgram') as HTMLSelectElement
-    ).value as
-      | 'fat loss'
-      | 'senior fitness'
-      | 'muscle gain'
-      | 'pre/postnatal fitness'
-      | 'contest preparation'
-      | 'overall fitness';
-    const phoneNumber = (
-      document.getElementById('phoneNumber') as HTMLInputElement
-    ).value;
-    const address = (document.getElementById('address') as HTMLInputElement)
-      .value;
-    const email =
-      (document.getElementById('email') as HTMLInputElement).value || undefined;
-    const joinedDate = new Date(
-      (document.getElementById('joinedDate') as HTMLInputElement).value
-    );
-    const endingDate = new Date(
-      (document.getElementById('endingDate') as HTMLInputElement).value
-    );
-    const specialHealthNotes =
-      (document.getElementById('specialHealthNotes') as HTMLTextAreaElement)
-        .value || undefined;
-    const isVIP = (document.getElementById('isVIP') as HTMLInputElement).checked;
+}
 
-    if (clients.filter(client => client.clientID == clientID).length != 0) {
-          displayMessage('Client ID must be unique!', 'error');
-          return;
-    }
-  
-  
-    const client = new Client(
-      isVIP,
-      clientID,
-      name,
-      DOB,
-      gender,
-      fitnessProgram,
-      { phoneNumber, address, email },
-      joinedDate,
-      endingDate,
-      specialHealthNotes
-    );
-  
-    clients.push(client);
-    displayClients();
-    displayMessage('Client added successfully!', 'success');
-  }
-  
-  // Display all clients
-  function displayClients(): void {
-    const display = document.getElementById('clientList')!;
-    display.innerHTML = clients
-      .map(client => client.displayClientInfo())
-      .join('');
-  }
-  
-  // Display VIP clients
-  function displayVIPClients(): void {
-    const display = document.getElementById('clientList')!;
-    display.innerHTML = clients
-      .filter(client => client.isVIP)
-      .map(client => client.displayClientInfo())
-      .join('');
+// Database of clients
+let clients: Client[] = [];
+
+// Add client
+function addClient(): void {
+  const clientID = Number(
+    (document.getElementById('clientID') as HTMLInputElement).value
+  );
+  const name = (document.getElementById('name') as HTMLInputElement).value;
+  const DOB = new Date(
+    (document.getElementById('dob') as HTMLInputElement).value
+  );
+  const gender = (document.getElementById('gender') as HTMLSelectElement)
+    .value as 'Male' | 'Female' | 'Unspecified';
+  const fitnessProgram = (
+    document.getElementById('fitnessProgram') as HTMLSelectElement
+  ).value as
+    | 'fat loss'
+    | 'senior fitness'
+    | 'muscle gain'
+    | 'pre/postnatal fitness'
+    | 'contest preparation'
+    | 'overall fitness';
+  const phoneNumber = (
+    document.getElementById('phoneNumber') as HTMLInputElement
+  ).value;
+  const address = (document.getElementById('address') as HTMLInputElement)
+    .value;
+  const email =
+    (document.getElementById('email') as HTMLInputElement).value || undefined;
+  const joinedDate = new Date(
+    (document.getElementById('joinedDate') as HTMLInputElement).value
+  );
+  const endingDate = new Date(
+    (document.getElementById('endingDate') as HTMLInputElement).value
+  );
+  const specialHealthNotes =
+    (document.getElementById('specialHealthNotes') as HTMLTextAreaElement)
+      .value || undefined;
+  const isVIP = (document.getElementById('isVIP') as HTMLInputElement).checked;
+
+  if (clients.filter(client => client.clientID == clientID).length != 0) {
+    displayMessage('Client ID must be unique!', 'error');
+    return;
   }
 
-  // case insensitive search of n-number properties of type T
-  // returns true if at least one of the property values includes the query value
-  function clientIDSearch(id: number): void{
-    const display = document.getElementById('clientList')!;
-    display.innerHTML = clients
-      .filter(client => client.clientID == id)
-      .map(client => client.displayClientInfo())
-      .join('');
-  }
 
-  
-  // Display a message
-  function displayMessage(message: string, type: 'success' | 'error'): void {
-    const msgDiv = document.createElement('div');
-    msgDiv.textContent = message;
-    msgDiv.style.color = type === 'success' ? 'green' : 'red';
-    document.body.prepend(msgDiv);
-    setTimeout(() => msgDiv.remove(), 3000);
-  }
+  const client = new Client(
+    isVIP,
+    clientID,
+    name,
+    DOB,
+    gender,
+    fitnessProgram,
+    { phoneNumber, address, email },
+    joinedDate,
+    endingDate,
+    specialHealthNotes
+  );
 
-  //  Update client data
-  function updateClient(): void {
-    const clientID = Number(
-      (document.getElementById('clientID') as HTMLInputElement).value
-    );
-    const name = (document.getElementById('name') as HTMLInputElement).value;
-    const DOB = new Date(
-      (document.getElementById('dob') as HTMLInputElement).value
-    );
-    const gender = (document.getElementById('gender') as HTMLSelectElement)
-      .value as 'Male' | 'Female' | 'Unspecified';
-    const fitnessProgram = (
-      document.getElementById('fitnessProgram') as HTMLSelectElement
-    ).value as
-      | 'fat loss'
-      | 'senior fitness'
-      | 'muscle gain'
-      | 'pre/postnatal fitness'
-      | 'contest preparation'
-      | 'overall fitness';
-    const phoneNumber = (
-      document.getElementById('phoneNumber') as HTMLInputElement
-    ).value;
-    const address = (document.getElementById('address') as HTMLInputElement)
-      .value;
-    const email =
-      (document.getElementById('email') as HTMLInputElement).value || undefined;
-    const joinedDate = new Date(
-      (document.getElementById('joinedDate') as HTMLInputElement).value
-    );
-    const endingDate = new Date(
-      (document.getElementById('endingDate') as HTMLInputElement).value
-    );
-    const specialHealthNotes =
-      (document.getElementById('specialHealthNotes') as HTMLTextAreaElement)
-        .value || undefined;
-    const isVIP = (document.getElementById('isVIP') as HTMLInputElement).checked;
+  clients.push(client);
+
+  displayMessage('Client added successfully!', 'success');
+}
+
+// Display all clients
+function displayClients(): void {
+  const display = document.getElementById('clientList')!;
+  display.innerHTML = clients
+    .map(client => client.displayClientInfo())
+    .join('');
+}
+
+// Display VIP clients
+function displayVIPClients(): void {
+  const display = document.getElementById('clientList')!;
+  display.innerHTML = clients
+    .filter(client => client.isVIP)
+    .map(client => client.displayClientInfo())
+    .join('');
+}
+
+// case insensitive search of n-number properties of type T
+// returns true if at least one of the property values includes the query value
+function clientIDSearch(): void {
+  const id = (document.getElementById("searchQuery") as HTMLInputElement).value;
+  const display = document.getElementById('clientList')!;
+  display.innerHTML = clients
+    .filter(client => client.clientID.toString() == id)
+    .map(client => client.displayClientInfo())
+    .join('');
+}
+
+
+// Display a message
+function displayMessage(message: string, type: 'success' | 'error'): void {
+  const msgDiv = document.createElement('div');
+  msgDiv.textContent = message;
+  msgDiv.style.color = type === 'success' ? 'green' : 'red';
+  document.body.prepend(msgDiv);
+  setTimeout(() => msgDiv.remove(), 3000);
+}
+
+//Edit client
+function editClient(id: Number) {
+  document.querySelectorAll("#client-card-" + id.toString() + " *:disabled").forEach((input) => input.removeAttribute("disabled"))
+}
+
+//Update client
+function updateClient(id: Number) {
+  document.querySelectorAll("#client-card-" + id.toString() + " .editable").forEach((input) => input.setAttribute("disabled", ""))
+
+  const name = (document.getElementById('name-' + id.toString()) as HTMLInputElement).value;
   
-    for (let i in clients) {
-      if (i == clientID.toString()) {
-        displayMessage('Client ID must be unique!', 'error');
-        return;
-      }
-    }
-  
-    const client = new Client(
-      isVIP,
-      clientID,
-      name,
-      DOB,
-      gender,
-      fitnessProgram,
-      { phoneNumber, address, email },
-      joinedDate,
-      endingDate,
-      specialHealthNotes
-    );
-  
-    clients.push(client);
-    displayClients();
-    displayMessage('Client added successfully!', 'success');
-  }
+  const gender = (document.getElementById('gender-' + id.toString()) as HTMLSelectElement)
+    .value as 'Male' | 'Female' | 'Unspecified';
+  const fitnessProgram = (
+    document.getElementById('fitnessProgram-' + id.toString()) as HTMLSelectElement
+  ).value as
+    | 'fat loss'
+    | 'senior fitness'
+    | 'muscle gain'
+    | 'pre/postnatal fitness'
+    | 'contest preparation'
+    | 'overall fitness';
+  const phoneNumber = (
+    document.getElementById('phoneNumber-' + id.toString()) as HTMLInputElement
+  ).value;
+  const address = (document.getElementById('address-' + id.toString()) as HTMLInputElement)
+    .value;
+  const email =
+    (document.getElementById('email-' + id.toString()) as HTMLInputElement).value || undefined;
+  const specialHealthNotes =
+    (document.getElementById('specialHealthNotes-' + id.toString()) as HTMLTextAreaElement)
+      .value || undefined;
+  const isVIP = (document.getElementById('isVIP-' + id.toString()) as HTMLInputElement).checked;
+
+}
